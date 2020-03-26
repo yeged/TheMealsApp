6a9643678b44bb84c1a42107f83fe426bf86c5a2
yeged <yigityceylan@gmail.com>
2020-03-26 19:43:27 +0300

    Adding Tabs-based Navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,6 @@
 import {createStackNavigator} from "react-navigation-stack"
 import {createAppContainer} from "react-navigation"
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
 import Colors from "../constants/Colors"
 
@@ -7,6 +8,7 @@ import Colors from "../constants/Colors"
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealScreen from "../screens/CategoryMealScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoritesScreen from "../screens/FavoritesScreen"
 
 const MealsNavigator = createStackNavigator({
     Categories: CategoriesScreen, //Shorter
@@ -23,4 +25,9 @@ const MealsNavigator = createStackNavigator({
     }
 })
 
-export default createAppContainer(MealsNavigator)
+const MealsFavTabNav = createBottomTabNavigator({
+    Meals: MealsNavigator,
+    Favorites: FavoritesScreen
+})
+
+export default createAppContainer(MealsFavTabNav)
--- screens/FavoritesScreen.jsx
+++ screens/FavoritesScreen.jsx
@@ -3,7 +3,9 @@ import {View, Text, StyleSheet} from "react-native";
 
 
 function FavoritesScreen(){
-    
+    return(
+        <View style={styles.screen}><Text>Hellow</Text></View>
+    )
 }
 
 const styles = StyleSheet.create({
